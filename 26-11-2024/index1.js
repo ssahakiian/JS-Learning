9d914e4abd1c682f6d4f5532779797200c182b70
ssahakiian <sona.sahakiian@gmal.com>
2024-11-26 20:06:43 +0400

    Problem 2: Remove Duplicates

---
+++ 26-11-2024/index1.js
@@ -0,0 +1,19 @@
+// Given an array [1, 2, 2, 3, 4, 4, 5]:
+// Write a program to create a new array where duplicate elements are removed.
+// The result should be [1, 2, 3, 4, 5] with most optimal way.
+
+const arr = [1, 2, 2, 3, 4, 4, 5];
+const uniqueItems = [];
+
+const obj = {}
+
+for (let i = 0; i < arr.length; i++) {
+     let item = arr[i];
+     if (!obj[item]) {
+         obj[item] = true;
+         uniqueItems.push(item)
+     }
+}
+
+console.log(uniqueItems);
+

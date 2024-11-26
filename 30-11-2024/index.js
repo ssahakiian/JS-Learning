02053e929d62f1540ccfa202f9d9bc1499581373
ssahakiian <sona.sahakiian@gmal.com>
2024-11-26 23:58:48 +0400

    .Includes Method

---
+++ 30-11-2024/index.js
@@ -0,0 +1,15 @@
+//Search for a specific value in an array (6) and log the index of that value 
+//if it is found with includes method.
+
+const arr = [1, 4, 6, 'JS', '2'];
+const searchValue = 6;
+let findIndex;
+
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === searchValue && arr.includes(searchValue)) {
+        findIndex = i;
+        break;
+    }
+}
+
+console.log(findIndex, 'Find Index')

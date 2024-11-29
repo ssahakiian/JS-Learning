bf03d80a8789ce9308e188d8764cd8edd90f99c8
ssahakiian <sona.sahakiian@gmal.com>
2024-11-29 22:53:26 +0400

    Asterisks Problem

--- 30-11-2024/index1.js
+++ 30-11-2024/index1.js
@@ -1,2 +1,10 @@
 //Print a right-aligned triangle of asterisks (*), where the number of asterisks 
 //increases by one per line, starting from 1 up to 10.
+
+
+const n = 10;
+
+for (let i = 1; i <= n; i++) {
+     let asterisks = '*'.repeat(i);
+    console.log(asterisks);
+}

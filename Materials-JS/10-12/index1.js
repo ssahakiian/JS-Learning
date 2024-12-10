77492e7a5cb5227aba202b260f0eb93b2c10f44e
ssahakiian <sona.sahakiian@gmal.com>
2024-12-11 00:35:30 +0400

    Arrow functions

--- Materials-JS/10-12/index1.js
+++ Materials-JS/10-12/index1.js
@@ -1,62 +1,10 @@
-// const boo = x => x + x; // => arrow function with ONE argument, no need for ()
+const boo = x => x + x; // => arrow function with ONE argument, no need for ()
 
-// console.log(boo(20));
+console.log(boo(20));
 
-// //
+//
 
-// const foo = (x, y) => { // with MORE than one arguments () is needed
-//     return x + y
-// }
-
-// console.log(foo(20, 30));
-
-// //
-
-
-// function buyTicket() {
-//     console.log('ticket price $1000');
-// }
-
-
-// function bookHotel (hotelName, foo) {
-//     if (hotelName === 'x') {
-//         foo 
-//     }
-
-// }
-// bookHotel('x', buyTicket);
-
-// const arr = [1, 2, 4, 5];
-
-// let max  = arr[0];
-// for (let i = 0; i < arr.length; i++) {
-//     if (arr[i] === undefined)
-//         continue
-// }
-
-// if (arr[i] > maxx) {
-//     max  = arr[i];
-// }
-
-
-
-
-const number = [1, 2, 4, 5];
-
-let max  = number[0];
-function forEach(arr, callback) {
-    for(let i = 0; i < arr.length; i++) {
-        if (arr[i] === undefined) {
-            continue
-        }
-        callback(arr[i], i);
-    }
+const foo = (x, y) => { // with MORE than one arguments () is needed
+    return x + y
 }
-
-forEach(number, function(item, index) {
-    if (item > max) {
-        max = item;
-    }
-});
-
-console.log(max);
+console.log(foo(20, 30));
